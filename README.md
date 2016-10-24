631d322b73ca64946554da2f4503a51c4a8c914d
Karl Gustav <kkgg84+git@gmail.com>
2016-10-24 17:37:15 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Bare minimum lib for making http requests from javascript.
+
+## Why?
+There are many libraries like this, but I couldn't find one that you
+could set the headers of the request. So I made another one...
+
+## How to make me add features:
+No
+
+## But!
+NO!!!
+
+## Usage
+
+    picoajax({
+        url: '/',
+        method: 'GET', // or POST, PUT, DELETE, and so on
+        data: '<some data>',
+        headers: {
+            key: '<value>'
+        }
+    })
+    .done(function(body, response) {
+        ...
+    })
+    .error(function(error, response) {
+        ....
+    })
+    .finally(function(error, response) {
+        ...
+    });
+
+## Browser support
+IE9+
---
+++ atto-ajax.js
@@ -0,0 +1,50 @@
+(function(global) {
+    global.atto = function(config) {
+        var _doneHandler, _errorHandler, _finallyHandler;
+        var request = new XMLHttpRequest();
+        request.open(config.method || 'GET', config.url, true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                _doneHandler && _doneHandler(request.responseText, request);
+            } else {
+                _errorHandler && _errorHandler(
+                    new Error(request.responseText), request
+                );
+            }
+            _finallyHandler && _finallyHandler(null, request)
+        };
+
+        for (var key in config.headers) {
+            if (config.headers.hasOwnProperty(key)) {
+                request.setRequestHeader(key, config.headers[key]);
+            }
+        }
+
+        request.onerror = function() {
+            var e = new Error('Connection error');
+            _errorHandler && _errorHandler(error);
+            _finallyHandler && _finallyHandler(error)
+        };
+        setTimeout(function() {
+            request.send(config.data);
+        });
+
+
+        return {
+            done: function (doneHandler) {
+                _doneHandler = doneHandler;
+                return this;
+            },
+            error: function (errorHandler) {
+                _errorHandler = errorHandler;
+                return this;
+            },
+            finally: function (finallyHandler) {
+                _finallyHandler = finallyHandler;
+                return this;
+            }
+        };
+    }
+}(window))
+
